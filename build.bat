07a6e3b612a255c1d6fe1efebcb288b78aa380fe
jfiala <Joh Admin2@JohAdmin2-PC>
2013-06-08 21:06:41 +0200

    add basic build skript

---
+++ build.bat
@@ -0,0 +1,31 @@
+@echo off
+ 
+setlocal
+ 
+set APP_NAME="thucydides-formatters"
+set CHROME_PROVIDERS="content"
+ 
+set ZIP_APP="c:\program files (x86)\7-zip\7z.exe"
+
+set ROOT_DIR=%CD%
+set TMP_DIR="build"
+ 
+rem remove any left-over files from previous build
+del /Q %APP_NAME%.xpi
+del /S /Q %TMP_DIR%
+ 
+mkdir %TMP_DIR%\chrome\content
+ 
+robocopy chrome\content %TMP_DIR%\chrome\content /E
+copy install.rdf %TMP_DIR%
+copy chrome.manifest %TMP_DIR%\chrome.manifest
+ 
+rem generate the XPI file
+cd %TMP_DIR%
+echo "Generating %APP_NAME%.xpi..."
+ 
+%ZIP_APP% a -r -y -tzip %APP_NAME%.zip *
+ 
+rename %APP_NAME%.zip %APP_NAME%.xpi
+ 
+endlocal
